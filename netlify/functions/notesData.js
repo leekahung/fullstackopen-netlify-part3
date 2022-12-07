c39c310a4af5851217897f0e0c14ecc7f7b8ae1f
Ka Hung Lee <lee.kahung414@gmail.com>
2022-12-07 04:56:16 -0800

    Including simple serverless function called notesData.js

---
+++ netlify/functions/notesData.js
@@ -0,0 +1,39 @@
+export const notes = async () => {
+  let notesData = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-1-17T17:30:31.098Z",
+      important: true,
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      date: "2022-1-17T18:39:34.091Z",
+      important: false,
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2022-1-17T19:20:14.298Z",
+      important: true,
+    },
+    {
+      id: 4,
+      content: "POST is used to add data to a REST api",
+      date: "2022-1-17T19:24:12.029Z",
+      important: false,
+    },
+    {
+      id: 5,
+      content: "Network tab of devtools is most beneficial",
+      date: "2022-1-17T20:05:51.764Z",
+      important: false,
+    },
+  ];
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(notesData),
+  };
+};
