63ecb352e549f08fe325dcb849d3914f6c26f629
Ka Hung Lee <lee.kahung414@gmail.com>
2022-12-12 17:51:48 -0800

    Replacing hardcoded data from data.js with data from MongoDB using mongoose with the file /models/note.js and modifying data.js to fetch data from MongoDB/mongoose via axios

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -2,39 +2,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const morgan = require("morgan");
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-1-17T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    date: "2022-1-17T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-1-17T19:20:14.298Z",
-    important: true,
-  },
-  {
-    id: 4,
-    content: "POST is used to add data to a REST api",
-    date: "2022-1-17T19:24:12.029Z",
-    important: false,
-  },
-  {
-    id: 5,
-    content: "Network tab of devtools is most beneficial",
-    date: "2022-1-17T20:05:51.764Z",
-    important: false,
-  },
-];
+const Note = require("../../models/note");
 
 const app = express();
 app.use(cors());
@@ -45,33 +13,45 @@ const middlewareLog = ":method :url :status :res[content-length] - ms :body";
 app.use(morgan(middlewareLog));
 
 app.get("/api/data", (request, response) => {
-  response.send(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/data/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((n) => n.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.post("/api/data", (request, response) => {
-  const note = request.body;
-  notes = notes.concat(note);
+  const body = request.body;
 
-  response.json(note);
+  if (body.content === undefined) {
+    return response.status(404).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    date: new Date(),
+    important: body.important,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.put("/api/data/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const updatedNote = request.body;
-  notes = notes.map((n) => (n.id !== id ? n : updatedNote));
+  const body = request.body;
 
-  response.json(updatedNote);
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { important: body.important },
+    { new: true }
+  ).then((updatedNote) => {
+    response.json(updatedNote);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -80,4 +60,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-module.exports.handler = serverless(app);
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  const result = await handler(event, context);
+  return result;
+};
