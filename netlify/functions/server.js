63e243ca58e75b3321e0f517ed6ed1bcb862af8b
Ka Hung Lee <lee.kahung414@gmail.com>
2022-12-19 03:32:07 -0800

    Recombining app.js with server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,8 +1,38 @@
-const app = require("./app");
-const serverless = require("serverless-http");
+const express = require("express");
+require("express-async-errors");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info("Connecting to MongoDB...");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB.");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error.message);
+  });
+
+app.use(middleware.middlewareLogger);
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+const serverless = require("serverless-http");
 const handler = serverless(app);
 module.exports.handler = async (event, context) => {
   const result = await handler(event, context);
   return result;
-};
+}
